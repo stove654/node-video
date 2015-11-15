3a79b5afcece878f756b4771f0ce7af383d2f483
Hoang Loi <hoangloi@Hoangs-MacBook-Pro.local>
2015-11-15 11:57:39 +0700

    update code

--- config/express.js
+++ config/express.js
@@ -5,12 +5,37 @@
 'use strict';
 
 var cors = require('cors');
+var multer  = require('multer');
+
 module.exports = function(app) {
 
   app.use(cors());
+
+  var done=false;
+  app.use(multer({ dest: './uploads/',
+    rename: function (fieldname, filename) {
+      console.log(filename);
+      return filename;
+    },
+    onFileUploadStart: function (file) {
+    },
+    onFileUploadComplete: function (file) {
+      done=true;
+      console.log("File uploaded.");
+    }
+  }));
 
   app.use(function(req, res, next) {
     res.contentType('application/json');
     next();
   });
+
+  app.route('/images')
+    .get(function (req, res) {
+      res.json({message: 'hello'})
+    })
+    .post(function( req, res) {
+      console.log('uploaded')
+    });
+
 };
